4f6951792a2aa85762cd4676dff0bf6392dccfc1
Gerry Mie <gerrymie30.l@gmail.com>
2023-07-18 17:24:14 +0800

    dall-e

--- app/dall-e/page.tsx
+++ app/dall-e/page.tsx
@@ -23,7 +23,7 @@ const DallE = () => {
     return (
         <div className='min-h-screen flex justify-center pt-10'>
             <div className='max-w-[600px]'>
-                <p className='py-5 text-xl'>Image Generator</p>
+                <p className='py-5 text-xl'>Next Image Generator</p>
                 <form onSubmit={generate}>
                     <input name='prompt' className='w-full min-w-[512px] p-2'/>
                     <button className='p-2 mt-4 w-full bg-blue-400' type='submit'>submit</button>
