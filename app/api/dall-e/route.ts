cb34e5e8dd5b6afb8a2b802d497fa33aff1f1942
Gerry Mie <gerrymie30.l@gmail.com>
2023-07-18 18:00:37 +0800

    dall-e

--- app/api/dall-e/route.ts
+++ app/api/dall-e/route.ts
@@ -5,13 +5,14 @@ import error = Simulate.error;
 export async function POST(req: Request) {
     const { prompt } = await req.json()
     try {
-        const response = await openai.createImage({
-            prompt,
-            n: 1,
-            size: '512x512'
-        })
-        const image_url = response.data.data[0].url
-        return new Response(image_url)
+        // const response = await openai.createImage({
+        //     prompt,
+        //     n: 1,
+        //     size: '512x512'
+        // })
+        // const image_url = response.data.data[0].url
+        // return new Response(image_url)
+        return new Response('')
     }catch (e){
        return new Response('error', {status:400})
     }
--- app/dall-e/page.tsx
+++ app/dall-e/page.tsx
@@ -27,7 +27,7 @@ const DallE = () => {
             <div className='max-w-[600px]'>
                 <p className='py-5 text-xl'>Next Image Generator</p>
                 <form onSubmit={generate}>
-                    <input name='prompt' className='w-full min-w-[512px] p-2'/>
+                    <input disabled name='prompt' className='w-full min-w-[512px] p-2'/>
                     <button className='p-2 mt-4 w-full bg-blue-400' type='submit'>submit</button>
                 </form>
                 <h1 className='py-5'>{status==='loading'?'generating...':status=='error'?'error':''}</h1>
